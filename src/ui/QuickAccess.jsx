164051d7066d518205a513b37072741d9d663d6c
Dandrizzy <dandrizzy4all@gmail.com>
2024-04-10 12:01:42 +0100

    m

--- src/ui/QuickAccess.jsx
+++ src/ui/QuickAccess.jsx
@@ -9,31 +9,31 @@ const QuickAccess = () => {
     <p className=" text-xs font-bold pt-2">Transfer Money</p>
    </div>
    <div className=" bg-slate-100 p-4 rounded-sm grid place-items-center gap-2">
-    <SiZelle className=" size-10" />
+    <SiZelle className=" text-purple-700 size-10" />
     <p className=" text-xs font-bold pt-2 text-center ">Send Money with Zelle</p>
    </div>
    <div className=" bg-slate-100 p-4 rounded-sm grid place-items-center gap-2">
-    <FaMoneyCheck className=" size-10" />
+    <FaMoneyCheck className=" text-green-600 size-10" />
     <p className=" text-xs font-bold pt-2">Deposit Check</p>
    </div>
    <div className=" bg-slate-100 p-4 rounded-sm grid place-items-center gap-2">
-    <FaHandHoldingHeart className=" size-10" />
+    <FaHandHoldingHeart className=" text-red-700 size-10" />
     <p className=" text-xs font-bold pt-2">Donate</p>
    </div>
-   <div className=" bg-slate-100 p-4 rounded-sm grid place-items-center gap-2">
-    <FaTicket className=" size-10" />
+   <div className=" bg-slate-100  p-4 rounded-sm grid place-items-center gap-2">
+    <FaTicket className=" text-neutral-900 size-10" />
     <p className=" text-xs font-bold pt-2">Pay Bills</p>
    </div>
    <div className=" bg-slate-100 p-4 rounded-sm grid place-items-center gap-2">
-    <FaWallet className=" size-10" />
+    <FaWallet className=" text-neutral-900 size-10" />
     <p className=" text-xs font-bold pt-2">Wallet</p>
    </div>
    <div className=" bg-slate-100 p-4 rounded-sm grid place-items-center gap-2">
-    <FaPaypal className=" size-10" />
+    <FaPaypal className=" text-blue-700 size-10" />
     <p className=" text-xs font-bold pt-2">PayPal</p>
    </div>
    <div className=" bg-slate-100 p-4 rounded-sm grid place-items-center gap-2">
-    <FaBitcoin className=" size-10" />
+    <FaBitcoin className=" text-yellow-500 size-10" />
     <p className=" text-xs font-bold pt-2">Bitcoin</p>
    </div>
 
